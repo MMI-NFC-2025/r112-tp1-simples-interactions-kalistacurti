8cae3d4758094ed649d5b49bfcfa15a90f2dd026
kalistacurti <kalista.curti@gmail.com>
2025-11-07 15:30:25 +0100

    travail fini

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,28 @@
 // TODO
+// ----------------------
+// Mettre le texte en rouge
+// ----------------------
+const paragraphe = document.querySelector("#a-mettre-en-rouge");
+paragraphe.style.color = "red";
+
+// ----------------------
+// Réagir à un clic sur le bouton
+// ----------------------
+const bouton = document.querySelector("#en-rouge-suite-a-click");
+
+bouton.addEventListener("click", () => {
+  bouton.style.color = "red";
+});
+
+// ----------------------
+// Réagir à un clic sur tous les H2
+// ----------------------
+const tousLesH2 = document.querySelectorAll("h2");
+
+tousLesH2.forEach((elem) => {
+  elem.addEventListener("click", (evt) => {
+    // evt.target = l’élément sur lequel on a cliqué
+    evt.target.style.color = "red";
+    console.log("Clic sur :", evt.target);
+  });
+});
